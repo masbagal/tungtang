5e712f0280fdf57f51636ea8061f50c81f7b7791
masbagal <dimitrij.agal@gmail.com>
2022-02-11 10:29:55 +0700

    sw exp

--- src/sw.js
+++ src/sw.js
@@ -1,12 +1,14 @@
 import { manifest, version } from "@parcel/service-worker";
 
 async function install() {
+  self.skipWaiting();
   const cache = await caches.open(version);
   await cache.addAll(manifest);
 }
 addEventListener("install", (e) => e.waitUntil(install()));
 
 async function activate() {
+  await clients.claim();
   const keys = await caches.keys();
   await Promise.all(keys.map((key) => key !== version && caches.delete(key)));
 }
